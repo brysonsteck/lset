b3e1bd3a1dfdf45fb3d735b3ba9912c66c37e3b9
Bryson Steck <steck.bryson@gmail.com>
2022-03-11 15:25:33 -0700

    got python bot working

---
+++ follow-src/source.py
@@ -0,0 +1,25 @@
+import requests
+import time
+import json
+
+with open('settings.json') as f:
+  settings = json.load(f)
+
+FOLLOWER_GOAL = settings['follower_goal']
+SUB_GOAL = settings['sub_goal']
+WRITE_FILE = settings['write_file']
+USER = settings['username']
+UPDATE = settings['seconds_interval']
+
+print("Running!")
+while(True):
+    followers = open(WRITE_FILE, 'w')
+    url_follows = "https://api.crunchprank.net/twitch/followcount/" + USER
+    read_follows = requests.get(url_follows)
+    url_subs = "https://decapi.me/twitch/subcount/" + USER
+    read_subs = requests.get(url_subs)
+    #if int(read_follows.text) > followerGoal:
+    #    followerGoal += 50
+    print(f"Subs: {read_subs.text}/{SUB_GOAL}\nFollower goal: {read_follows.text}/{FOLLOWER_GOAL}\n", end='', file=followers)
+    followers.close()
+    time.sleep(UPDATE)
