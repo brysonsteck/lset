54037847b6a81c3a44ed433e7c4219115c1b4245
Bryson Steck <bryson@donald.local>
2022-03-11 17:08:02 -0700

    fixed install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 # check if correct programs are installed
 type git npm node python3 pip3
+ERROR="$?"
 
-if [[ $! -ne 0 ]]; then 
+if [[ ERROR -ne 0 ]]; then 
   echo "install: one or more of the above programs are not installed. the programs in question have lines that end in \"not found\". please make sure that these programs are installed through your package manager"
   exit 0
 else
